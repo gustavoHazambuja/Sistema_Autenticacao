b0ed8304dcc0d74538746befdfff0bbf06c3e922
Gustavo Henrique <guazambujinha@gmail.com>
2025-05-05 17:07:58 -0300

    estrutura feita

---
+++ autenticacao/src/main/java/com/example/Service/Autenticacao.java
@@ -0,0 +1,17 @@
+package com.example.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Autenticacao {
+    
+    private Map<String, String> usuarios;
+
+    public Autenticacao(){
+        this.usuarios = new HashMap<String, String>();
+    }
+
+    
+
+    
+}
